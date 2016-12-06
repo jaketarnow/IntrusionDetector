536773b3bf0377e20c70575b8fe1748a533206c1
dmansilva <djsilva@usfca.edu>
2016-12-06 13:38:14 -0800

    Add KNN template

--- intrusion-detector.R
+++ intrusion-detector.R
@@ -380,6 +380,28 @@ mean(lda.pred$class == train$connection_type)
 proc.time() - lda.time
 
 
+?knn
+attach(Smarket)
+## Lag1/2 are predictions so we need to replace with our own.
+Xlag=cbind(Lag1,Lag2)
+## training set 
+train= ...
+
+## we should perform KNN on number of neighbours (k) = 1 first, more flexible fit giving low variance but high bias
+knn.pred=knn(Xlag[train,],Xlag[!train,],Direction[train],k=1)
+table(knn.pred,Direction[!train])
+mean(knn.pred==Direction[!train])
+
+## KNN on k = 9 which is a high k value, provides low flexibility (high variance) but low bias
+knn.pred=knn(Xlag[train,],Xlag[!train,],Direction[train],k=9)
+table(knn.pred,Direction[!train])
+mean(knn.pred==Direction[!train])
+
+## Depending on results of 1 and 9 we should run a k value in between to see if we get better results
+knn.pred=knn(Xlag[train,],Xlag[!train,],Direction[train],k=9)
+table(knn.pred,Direction[!train])
+mean(knn.pred==Direction[!train])
+
 # TODO:
 # Supervised
 #   KNN
